1fad55c7cf9dc478d2a1b1f1e07391cb0144fef3
andip71 <andreasp@gmx.de>
2014-09-22 16:13:07 +0200

    cpufreq: Add wheatley governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -436,6 +436,9 @@ extern struct cpufreq_governor cpufreq_gov_yankactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
 #endif
 
 
